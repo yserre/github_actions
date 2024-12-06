061ac0c5728fdf1536ea3ba1575c92625a2527c6
Ubuntu <ubuntu@ip-172-31-2-152.eu-west-1.compute.internal>
2024-12-06 09:11:21 +0000

    Ajout Test

--- .github/workflows/file_test.py
+++ .github/workflows/file_test.py
@@ -1,26 +1,6 @@
 import pytest
 
 def test_calc_addition():
-<<<<<<< HEAD
-	# Fonction test du résultat de 2+4
-	output = 2+4
-	assert output == 6
-
-def test_calc_substraction(): 
-	# Fonction test du résultat de 2-4
-    	output = 2-4
-    	assert output == -2
-
-def test_calc_multiply():
-  	# Fonction test du résultat de 2*4
-    	output = 2*4
-    	assert output == 8
-
-def test_coucou():
-  	# Fonction test si la résultat renvoie 'hello'
-    	output='hello'
-    	assert output == 'hello'
-=======
   # Fonction test du résultat de 2+4
   output = 2+4
   assert output == 6
@@ -39,4 +19,4 @@ def test_coucou():
   # Fonction test si la résultat renvoie 'hello'
   output='hello'
   assert output == 'hello'
->>>>>>> 5c3978ad0354efd18d7c35b24528180eb046b065
+
